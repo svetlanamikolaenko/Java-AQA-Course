032d01920067954d31244b7d45ca7a95ef9025ea
svetlanamikolaenko <svetlana.mikolaenko@gmail.com>
2021-03-12 09:56:14 +0200

    print char unicode

--- src/homework1/HelloUnicode.java
+++ src/homework1/HelloUnicode.java
@@ -6,10 +6,13 @@ public class HelloUnicode {
         char e = 101;
         char l = 108;
         char o = 111;
-        System.out.println(((int) H));
-        System.out.println(((int) e));
-        System.out.println(((int) l));
-        System.out.println(((int) o));
+
+        //OR
+        System.out.println(((int) 'H'));
+        System.out.println(((int) 'e'));
+        System.out.println(((int) 'l'));
+        System.out.println(((int) 'l'));
+        System.out.println(((int) 'o'));
         
     }
 }
