a3137a27d37a0c15e30f7d9584260e9c27de33a8
svetlanamikolaenko <svetlana.mikolaenko@gmail.com>
2021-03-18 16:12:32 +0200

    Sort Array

--- src/homework2/ArrayBubbleSort.java
+++ src/homework2/ArrayBubbleSort.java
@@ -2,7 +2,7 @@ package homework2;
 
 import java.util.Arrays;
 
-public class SortArray {
+public class ArrayBubbleSort {
     /* Create an array of any length and fill it with random integers.
     Using loops and conditions sort this array and print it out to the console.
      You can use any sorting algorithm you want. Do not use build-in methods to sort the array.
@@ -12,18 +12,16 @@ public class SortArray {
     for (int i = 0; i<array.length; i++){
         array[i] = (int) Math.round(Math.random() * 100);
     }
-        System.out.println(Arrays.toString(array));
+        System.out.println("Unsorted Array: " + Arrays.toString(array));
 
-    int min = 0;
     for (int i = 0; i<array.length; i++){
-        for (int j = 0; j < array.length; j++)
-            if(array[i] < j){
-                int temp = min;
-                min = array[i];
-                array[i] = temp;
+        for (int j = 0; j < array.length-1; j++)
+            if(array[j] > array[j+1] ){
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
             }
     }
-        System.out.println(Arrays.toString(array));
-
+        System.out.println("Sorted array: " + Arrays.toString(array));
     }
 }
