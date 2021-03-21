670af9c34990cf7cff21b5bfcbeee49f20844766
svetlanamikolaenko <svetlana.mikolaenko@gmail.com>
2021-03-21 14:24:03 +0200

    add dot to string

--- src/homework3/StartsFromThe.java
+++ src/homework3/StartsFromThe.java
@@ -11,18 +11,19 @@ public class StartsFromThe {
         String word2 = "End The";
 
         String[] result1 = word1.split(" ");
-        String[] result2 = word2.split("");
+        String[] result2 = word2.split(" ");
 
         if (result1[0].compareTo("The") == 0){
             System.out.println(word1 + " stars from \"The\"");
         }
 
+        //OR
         System.out.println( word1 + " starts from The - > " + result1[0].matches("The"));
         System.out.println( word2 + " starts from The - > " + result2[0].matches("The"));
 
+        //OR
         System.out.println(checkStartWithThe(word1));
         System.out.println(checkStartWithThe(word2));
-
     }
 
     public static String checkStartWithThe(String words){
@@ -32,5 +33,4 @@ public class StartsFromThe {
         }
         return "Text starts from \"" + strings[0] + "\"";
     }
-
 }
