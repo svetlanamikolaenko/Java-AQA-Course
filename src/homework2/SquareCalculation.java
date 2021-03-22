3b9e716b83716607f9b54c2f72eaf4c5d41d0373
svetlanamikolaenko <svetlana.mikolaenko@gmail.com>
2021-03-22 09:18:11 +0200

    remove random values generation

--- src/homework2/SquareCalculation.java
+++ src/homework2/SquareCalculation.java
@@ -6,26 +6,12 @@ public class SquareCalculation {
      */
     public static void main(String[] args) {
         //Square of rectangle
-        int length;
-        int width;
-
-        do {
-            length = (int) Math.round(Math.random() * 10);
-        } while (length == 0);
-
-        do {
-            width = (int) Math.round(Math.random() * 10);
-        } while (width == 0);
-
+        int length = 10;
+        int width = 6;
         System.out.println("Square of rectangle is: "  + width + " * " + length + " = " + Math.multiplyExact(length, width));
 
         //Square of circle
-        int radius;
-
-        do {
-            radius = (int) Math.round(Math.random() * 10);
-        } while (radius == 0);
-
+        int radius = 8;
         System.out.println("Square of circle is: "  +  radius  + "^2"+ " * " + Math.PI +
                 " = " + (Math.round(Math.PI * Math.pow(radius, 2)*100.0)/100.00));
     }
