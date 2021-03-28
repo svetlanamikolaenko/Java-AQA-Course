8b5630e9f5a8e6f361fdc7caa81dd2dbb8fd003e
svetlanamikolaenko <svetlana.mikolaenko@gmail.com>
2021-03-28 15:54:44 +0300

    task 10 overloading

---
+++ src/homewok4/task10/Main.java
@@ -0,0 +1,11 @@
+package homewok4.task10;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Rectangle square (double) was called: " + rectangle.square(10.6, 4.4));
+        System.out.println("Rectangle square (int) was called: " + rectangle.square(5, 7));
+    }
+
+}
---
+++ src/homewok4/task10/Rectangle.java
@@ -0,0 +1,12 @@
+package homewok4.task10;
+
+public class Rectangle {
+
+    public  int square(int width, int height){
+        return width * height;
+    }
+
+    public double square(double width, double height){
+        return width * height;
+    }
+}
