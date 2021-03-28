6ede9760da7a2b6535448e5913d1ada7c7c30970
svetlanamikolaenko <svetlana.mikolaenko@gmail.com>
2021-03-28 16:44:52 +0300

    task 4 staticCalculator

--- src/homewok4/task10/Rectangle.java
+++ src/homewok4/task10/Rectangle.java
@@ -1,4 +1,8 @@
 package homewok4.task10;
+/*
+10. Create a simple class with overloaded methods.
+Write another class to demonstrate the concept of methods overloading.
+ */
 
 public class Rectangle {
 
