e1200f637ddb714837dff4c43e68f79b28546e1b
svetlanamikolaenko <svetlana.mikolaenko@gmail.com>
2021-04-07 20:11:12 +0300

    task 3 handle all the possible exceptions

---
+++ src/homework8/task3/Main.java
@@ -0,0 +1,36 @@
+package homework8.task3;
+/*
+3. Finish the program to handle all the possible exceptions:
+public class Main {
+
+    public static void main(String[] args) {
+        int a = args.length;
+        System.out.println(a);
+
+        int b = 10 / a;
+        int[] c = {1};
+        System.out.println(c[1]);
+
+    }
+}
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        int a = args.length;
+        try {
+            System.out.println(a);
+            int b = 10 / a;
+            int[] c = {1};
+            System.out.println(c[1]);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Division by 0");        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Array index not exists");
+        }
+        catch (Exception e) {
+            System.out.println("Exception");
+        }
+    }
+}
